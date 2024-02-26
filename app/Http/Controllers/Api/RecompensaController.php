c9a49a004795a75a743fb6c617d7d015a7f0d5b7
Joelpr27 <joel.perez.esmc@gmail.com>
2024-02-26 20:02:15 +0100

    Controller recompensas, updarte y destroy

--- app/Http/Controllers/Api/RecompensaController.php
+++ app/Http/Controllers/Api/RecompensaController.php
@@ -24,9 +24,36 @@ class RecompensaController extends Controller
 
         $recompensa = $request->all();
 
-        $accion = Recompensa::create($recompensa);
+        $data = Recompensa::create($recompensa);
 
-        return response()->json(['success' => true, 'data' => $accion]);
+        return response()->json(['success' => true, 'data' => $data]);
+
+    }
+
+    public function update($id, Request $request){
+        $recompensa = Recompensa::find($id);
+
+        $request->validate([
+            'nombre' => 'required|max:15',
+            'descripcion' => 'required|max:150',
+            'precio' => 'required',
+            'imagen' => 'nullable',
+        ]);
+
+        $dataToUpdate = $request->all();
+
+        $recompensa->update($dataToUpdate);
+
+        return response()->json(['success' => true, 'data' => $recompensa]);
+
+    }
+
+    public function destroy($id){
+        $recompensa = Recompensa::find($id);
+
+        $recompensa->delete();
+
+        return response()->json(['success' => true, 'data' => 'Recompensa eliminada correctamente']);
 
     }
 }
