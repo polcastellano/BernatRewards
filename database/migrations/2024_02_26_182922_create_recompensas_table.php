6d203251fc6b5aee3398ebb78d3b1dbe11e19d2e
Pol <polcastellanofp@ibf.cat>
2024-02-26 19:36:28 +0100

    Recompensas creadas

--- database/migrations/2024_02_26_182922_create_recompensas_table.php
+++ database/migrations/2024_02_26_182922_create_recompensas_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('recompensas', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->double('precio');
+            $table->string('imagen')->nullable();
             $table->timestamps();
         });
     }
