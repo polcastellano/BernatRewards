85553e6347c6208ff3df979ff4cb2f99214f8212
Pol <polcastellanofp@ibf.cat>
2024-02-27 17:45:02 +0100

    Creacion de tabla N-M categorias_recompensas

---
+++ database/migrations/2024_02_27_163430_categorias_recompensas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categorias_recompensas', function (Blueprint $table) {
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('recompensa_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade');
+            $table->foreign('recompensa_id')->references('id')->on('recompensas')->onDelete('cascade');
+            $table->primary(['categoria_id', 'recompensa_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categorias_recompensas');
+
+    }
+};
