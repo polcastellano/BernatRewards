f90a3bdd3a899f8880cd43a982d88a84098884b3
Joelpr27 <joel.perez.esmc@gmail.com>
2024-02-28 18:41:29 +0100

    Factoy Usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\Factories\UserFactory;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,6 +12,9 @@ return new class extends Migration
      *
      * @return void
      */
+
+    protected $model = UserFactory::class;
+
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
