f71c684f723df7803fa5f0295d20e4708d1e12a0
Joelpr27 <joel.perez.esmc@gmail.com>
2024-03-01 16:22:16 +0100

    Logo con Titulo

--- resources/js/layouts/AppTopbar.vue
+++ resources/js/layouts/AppTopbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="layout-topbar">
         <router-link to="/" class="layout-topbar-logo">
-            <img src="/images/bernatRewards.svg" alt="logo" />
+            <img src="/images/bernatRewards_Titulo.svg" alt="logo" />
             <span></span>
         </router-link>
 
