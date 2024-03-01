a67b6c5cb0f3e7caad186139856ba1f44a7bc539
Joelpr27 <joel.perez.esmc@gmail.com>
2024-03-01 18:51:41 +0100

    Login y Diseño provisional

--- resources/js/views/login/Login.vue
+++ resources/js/views/login/Login.vue
@@ -3,13 +3,13 @@
         <div class="row justify-content-center my-5">
             <div class="col-md-6">
                 <div class="card border-0 shadow-sm">
+                    <div class="card-header bg-transparent"><h2>{{ $t('login') }}</h2></div>
                     <div class="card-body">
                         <form @submit.prevent="submitLogin">
                             <div class="">
                                 <!-- Email -->
                                 <div class="mb-3">
-                                    <label for="email" class="form-label">{{ $t('email') }}</label>
-                                    <input v-model="loginForm.email" id="email" type="email" class="form-control" required autofocus autocomplete="username">
+                                    <input v-model="loginForm.email" id="email" type="email" class="form-control inputLogin" required autofocus autocomplete="username" :placeholder="$t('email')">
                                     <!-- Validation Errors -->
                                     <div class="text-danger mt-1">
                                         <div v-for="message in validationErrors?.email">
@@ -19,33 +19,34 @@
                                 </div>
                                 <!-- Password -->
                                 <div class="mb-4">
-                                    <label for="password" class="form-label">
-                                        {{ $t('password') }}
-                                    </label>
-                                    <input v-model="loginForm.password" id="password" type="password" class="form-control" required autocomplete="current-password">
+                                    <input v-model="loginForm.password" id="password" type="password" class="form-control inputLogin" required autocomplete="current-password" :placeholder="$t('password')">
                                     <!-- Validation Errors -->
                                     <div class="text-danger-600 mt-1">
                                         <div v-for="message in validationErrors?.password">
                                             {{ message }}
                                         </div>
                                     </div>
+                                    <router-link :to="{name: 'auth.forgot-password'}" class="forgotPass">{{ $t('forgot_password')}}</router-link>
+
                                 </div>
-                                <!-- Remember me -->
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" v-model="loginForm.remember" id="flexCheckIndeterminate">
-                                    <label class="form-check-label" for="flexCheckIndeterminate">
-                                        {{ $t('remember_me') }}
-                                    </label>
+                                <div class="d-flex justify-content-between align-items-center mt-5">
+                                    <!-- Remember me -->
+                                    <div class="form-check p-0 align-items-center pl-4">
+                                        <input class="form-check-input" type="checkbox" name="remember" v-model="loginForm.remember" id="flexCheckIndeterminate">
+                                        <label class="form-check-label" for="flexCheckIndeterminate">
+                                            {{ $t('remember_me') }}
+                                        </label>
+                                    </div>
+
+                                    <!-- Buttons -->
+                                    <div class="flex align-items-center">
+                                        <button class="btn btn-primary" :class="{ 'opacity-25': processing }" :disabled="processing">
+                                            {{ $t('login') }}
+                                        </button>
+                                    </div>
                                 </div>
 
-                                <!-- Buttons -->
-                                <div class="flex items-center justify-end mt-4">
-                                    <button class="btn btn-primary" :class="{ 'opacity-25': processing }" :disabled="processing">
-                                        {{ $t('login') }}
-                                    </button>
-                                </div>
                             </div>
-                            <router-link :to="{name: 'auth.forgot-password'}">{{ $t('forgot_password')}}</router-link>
                         </form>
                     </div>
                 </div>
@@ -61,3 +62,27 @@ import useAuth from '@/composables/auth'
 const { loginForm, validationErrors, processing, submitLogin } = useAuth();
 
 </script>
+
+<style>
+
+.inputLogin{
+    background-color: #D9D9D9;
+    border-radius: 100px;
+}
+
+.inputLogin::placeholder{
+    font-weight: bold;
+    font-size: small;
+}
+
+.forgotPass{
+    color: #145A79;
+}
+
+button{
+    background-color: #145A79 !important;
+    border-radius: 100px !important;
+
+}
+
+</style>
--- resources/js/views/register/index.vue
+++ resources/js/views/register/index.vue
@@ -3,14 +3,13 @@
         <div class="row justify-content-center my-5">
             <div class="col-md-6">
                 <div class="card border-0 shadow-sm">
-                    <div class="card-header bg-transparent">{{ $t('register') }}</div>
+                    <div class="card-header bg-transparent"><h2>{{ $t('register') }}</h2></div>
                     <div class="card-body">
                         <form @submit.prevent="submitRegister">
                             <div class="">
                                 <!-- Email -->
                                 <div class="mb-3">
-                                    <label for="name" class="form-label">{{ $t('name') }}</label>
-                                    <input v-model="registerForm.name" id="name" type="text" class="form-control" autofocus>
+                                    <input v-model="registerForm.name" id="name" type="text" class="form-control inputRegister" autofocus :placeholder="$t('name')">
                                     <!-- Validation Errors -->
                                     <div class="text-danger mt-1">
                                         <div v-for="message in validationErrors?.name">
@@ -19,8 +18,7 @@
                                     </div>
                                 </div>
                                 <div class="mb-3">
-                                    <label for="email" class="form-label">{{ $t('email') }}</label>
-                                    <input v-model="registerForm.email" id="email" type="email" class="form-control" autocomplete="username">
+                                    <input v-model="registerForm.email" id="email" type="email" class="form-control inputRegister" autocomplete="username" :placeholder="$t('email')">
                                     <!-- Validation Errors -->
                                     <div class="text-danger mt-1">
                                         <div v-for="message in validationErrors?.email">
@@ -30,10 +28,7 @@
                                 </div>
                                 <!-- Password -->
                                 <div class="mb-4">
-                                    <label for="password" class="form-label">
-                                        {{ $t('password') }}
-                                    </label>
-                                    <input v-model="registerForm.password" id="password" type="password" class="form-control" autocomplete="current-password">
+                                    <input v-model="registerForm.password" id="password" type="password" class="form-control inputRegister" autocomplete="current-password" :placeholder="$t('password')">
                                     <!-- Validation Errors -->
                                     <div class="text-danger-600 mt-1">
                                         <div v-for="message in validationErrors?.password">
@@ -42,10 +37,7 @@
                                     </div>
                                 </div>
                                 <div class="mb-4">
-                                    <label for="password_confirmation" class="form-label">
-                                        {{ $t('confirm_password') }}
-                                    </label>
-                                    <input v-model="registerForm.password_confirmation" id="password_confirmation" type="password" class="form-control" autocomplete="current-password">
+                                    <input v-model="registerForm.password_confirmation" id="password_confirmation" type="password" class="form-control inputRegister" autocomplete="current-password" :placeholder="$t('confirm_password')">
                                     <!-- Validation Errors -->
                                     <div class="text-danger-600 mt-1">
                                         <div v-for="message in validationErrors?.password_confirmation">
@@ -55,7 +47,7 @@
                                 </div>
 
                                 <!-- Buttons -->
-                                <div class="flex items-center justify-end mt-4">
+                                <div class="flex justify-content-end align-items-center mt-5">
                                     <button class="btn btn-primary" :class="{ 'opacity-25': processing }" :disabled="processing">
                                         {{ $t('register') }}
                                     </button>
@@ -76,3 +68,27 @@ import useAuth from '@/composables/auth'
 const { registerForm, validationErrors, processing, submitRegister } = useAuth();
 
 </script>
+
+<style>
+
+.fondo{
+    background-color: red;
+}
+
+.inputRegister{
+    background-color: #D9D9D9;
+    border-radius: 100px;
+}
+
+.inputRegister::placeholder{
+    font-weight: bold;
+    font-size: small;
+}
+
+button{
+    background-color: #145A79 !important;
+    border-radius: 100px !important;
+
+}
+
+</style>
