419023bd67f41ec155db89b2c9644a924e74f2ae
Pol <polcastellanofp@ibf.cat>
2024-04-05 17:18:44 +0200

    Recompensas acabadas

--- resources/js/views/admin/recompensas/Edit.vue
+++ resources/js/views/admin/recompensas/Edit.vue
@@ -105,7 +105,7 @@
 
 
 <script setup>
-    import { onMounted, reactive, watchEffect } from "vue";
+    import { onMounted, reactive, watchEffect, watch, ref } from "vue";
     import { useRoute } from "vue-router";
     import { useForm, useField} from "vee-validate";
     import DropZone from "@/components/DropZone.vue";
@@ -116,16 +116,23 @@
     import usarRecompensas from "@/composables/recompensas";
 
     setLocale(es);
-    
 
     const schema = yup.object({
         nombre: yup.string().required().label('Nombre'),
         descripcion: yup.string().required().label('Descripcion'),
         precio: yup.number().integer().required().min(0).label('Precio'),
-        imagen: yup.mixed().required().label('Imagen').test('fileFormat', 'Solo se permiten PNG y JPG', (value)=> {
-            return value.type == 'image/jpeg' || value.type == 'image/png';
-        })
-        ,   
+        imagen: yup.mixed().test('fileFormat', 'Solo se permiten PNG y JPG', (value) => {
+            if (!value) {
+                // Si el valor está vacío, retornar un mensaje personalizado
+                    throw new yup.ValidationError('Imagen es un campo requerido', null, 'imagen');
+        } else if (typeof value === 'string') {
+                // Si es un string, es válido pero no debe estar vacío
+                return value.trim() !== ''; // Verificar que no esté vacío
+            } else {
+                // Si es un objeto de archivo, validar su tipo
+                return ['image/jpeg', 'image/png'].includes(value.type);
+            }
+        }).required().label('Imagen'),
         nivel_desbloqueo: yup.number().integer().required().min(0).max(5).label('Nivel'),
         categorias: yup.array().min(1).required().label('Categorias'),
     })
