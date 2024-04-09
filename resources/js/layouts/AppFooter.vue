7adfc8291f731aca529cdbcdde86e05fa96c27f8
Pol <polcastellanofp@ibf.cat>
2024-04-09 18:32:32 +0200

    Modificacion del footer, falta el responsive

--- resources/js/layouts/AppFooter.vue
+++ resources/js/layouts/AppFooter.vue
@@ -10,37 +10,59 @@ const logoUrl = computed(() => {
 </script>
 
 <template>
-    <div class="col d-flex bg-cyan-900 p-5">
-        <div class="col-4 d-flex">
+    <div class="container-fluid d-flex bg-cyan-900 p-5 flex-wrap justify-content-between">
+        <div class="col-sm-12 col-md-7 col d-flex justify-content-center mb-3">
             <div>
-                <iframe width="250" height="250" class="rounded-4 border-0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2992.329138143673!2d2.028863386729124!3d41.41037335131185!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49a9abc2c50f1%3A0x967c9dc9d440c41b!2sInstitut%20Bernat%20el%20Ferrer!5e0!3m2!1ses!2ses!4v1712599612391!5m2!1ses!2ses" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe width="250" height="250" class="rounded-4 border-0"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2992.329138143673!2d2.028863386729124!3d41.41037335131185!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49a9abc2c50f1%3A0x967c9dc9d440c41b!2sInstitut%20Bernat%20el%20Ferrer!5e0!3m2!1ses!2ses!4v1712599612391!5m2!1ses!2ses"
+                    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div class="d-flex flex-column ms-3 justify-content-around">
                 <div class="d-flex align-items-center">
-                    <i class="pi pi-map-marker text-footer" style="font-size: 1.5rem"></i>
+                    <i class="pi pi-map-marker text-white" style="font-size: 1.5rem"></i>
                     <p class="text-footer ms-2">{{ $t('ubicacion') }}</p>
                 </div>
                 <div class="d-flex align-items-center">
-                    <i class="pi pi-clock text-footer" style="font-size: 1.5rem"></i>
+                    <i class="pi pi-clock text-white" style="font-size: 1.5rem"></i>
                     <p class="text-footer ms-2">{{ $t('horario') }}</p>
                 </div>
                 <div class="d-flex align-items-center">
-                    <i class="pi pi-phone text-footer" style="font-size: 1.5rem"></i>
+                    <i class="pi pi-phone text-white" style="font-size: 1.5rem"></i>
                     <p class="text-footer ms-2">936683762</p>
                 </div>
             </div>
         </div>
-            
-        <div class="col-4 d-flex my-4 align-items-center justify-content-center">
-            <p class="text-footer">Bernat Rewards 2024®</p>
+
+        <div class="col-sm-12 col-md-7 col d-flex justify-content-center mb-3">
+            <div class="d-flex flex-column align-items-start">
+                <h2 class="text-footer">{{ $t('tituloPolit') }}</h2>
+                <ul class="list-group list-group-flush">
+                    <li class="text-footer bg-transparent border-0 list-group-item">{{ $t('avisLegal') }}</li>
+                    <li class="text-footer bg-transparent border-0 list-group-item">{{ $t('polPriv') }}</li>
+                    <li class="text-footer bg-transparent border-0 list-group-item">{{ $t('polCook') }}</li>
+                </ul>
+            </div>
         </div>
-        <div class="col-4 d-flex my-4 align-items-center justify-content-center">
-            <p class="text-footer">Bernat Rewards 2024®</p>
+
+
+        <div class="col-sm-12 col-md-7 col d-flex justify-content-center mb-3">
+            <div class="d-flex flex-column align-items-start">
+                <h2 class="text-footer">{{ $t('tituloPolit') }}</h2>
+                <ul class="list-group list-group-flush">
+                    <li class="text-footer bg-transparent border-0 list-group-item">{{ $t('avisLegal') }}</li>
+                    <li class="text-footer bg-transparent border-0 list-group-item">{{ $t('polPriv') }}</li>
+                    <li class="text-footer bg-transparent border-0 list-group-item">{{ $t('polCook') }}</li>
+                </ul>
+            </div>
         </div>
+    </div>
+    <div class="col d-flex bg-green-900 p-5 justify-content-between border-top-1">
+
     </div>
 </template>
 <style lang="scss" scoped>
 .text-footer{
     color: white;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 </style>
