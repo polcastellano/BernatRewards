51ce8238d16a5745f18f20eb803f2ea7ae6c14aa
Pol <polcastellanofp@ibf.cat>
2024-04-26 16:55:32 +0200

    Merge branch 'Pol'

--- resources/js/composables/profile.js
+++ resources/js/composables/profile.js
@@ -1,49 +1,75 @@
 import { ref, inject } from 'vue'
 import { useRouter } from 'vue-router'
-import store from "../store";
+import { userStore } from '@/store/authPinia';
 
 export default function useProfile() {
-    const profile = ref({
-        name: '',
-        email: '',
-    })
+    const profile = ref({})
 
     const router = useRouter()
     const validationErrors = ref({})
-    const isLoading = ref(false)
+    const cargando = ref(false)
     const swal = inject('$swal')
 
-    const getProfile = async () => {
-        profile.value = store.getters["auth/user"]
-        // axios.get('/api/user')
-        //     .then(({data}) => {
-        //         profile.value = data.data;
-        //     })
+    const getProfile = async (id) => {
+        axios.get('/api/perfil/' + id)
+        .then(response => {
+            console.log(response.data)
+            profile.value = response.data.data;
+        })
     }
 
-    const updateProfile = async (profile) => {
-        if (isLoading.value) return;
+    // const updateProfile = async (profile) => {
+    //     if (isLoading.value) return;
 
-        isLoading.value = true
-        validationErrors.value = {}
+    //     isLoading.value = true
+    //     validationErrors.value = {}
 
-        axios.put('/api/user', profile)
-            .then(({data}) => {
-                if (data.success) {
-                    store.commit('auth/SET_USER', data.data)
-                    // router.push({name: 'profile.index'})
-                    swal({
-                        icon: 'success',
-                        title: 'Profile updated successfully'
-                    })
-                }
+    //     axios.put('/api/user', profile)
+    //         .then(({data}) => {
+    //             if (data.success) {
+    //                 store.commit('auth/SET_USER', data.data)
+    //                 // router.push({name: 'profile.index'})
+    //                 swal({
+    //                     icon: 'success',
+    //                     title: 'Profile updated successfully'
+    //                 })
+    //             }
+    //         })
+    //         .catch(error => {
+    //             if (error.response?.data) {
+    //                 validationErrors.value = error.response.data.errors
+    //             }
+    //         })
+    //         .finally(() => isLoading.value = false)
+    // }
+
+    const updateProfile = async (user) => {
+        if (cargando.value) return;
+
+        cargando.value = true
+
+
+        axios.post('/api/profile/update/' + user, {
+            headers: {
+                "content-type": "multipart/form-data"
+            }
+        })
+        .then(response => {
+            console.log(response)
+            router.push({ name: 'perfil.edit' })
+            swal({
+                icon: 'success',
+                title: 'Perfil editado correctamente'
             })
-            .catch(error => {
-                if (error.response?.data) {
-                    validationErrors.value = error.response.data.errors
-                }
+        })
+        .catch(error => {
+            console.log(error)
+            swal({
+                icon: 'error',
+                title: 'Perfil editado incorrectamente'
             })
-            .finally(() => isLoading.value = false)
+        })
+        .finally(() => cargando.value = false)
     }
 
     return {
